f30a159a372a7130163ee60125883e75eac14314
Thamara Bhagya <thamarabhagya755@gmail.com>
2025-08-20 22:09:32 +0530

    missing parameter error fixed

--- Backend/server.js
+++ Backend/server.js
@@ -37,25 +37,11 @@ app.use((req, res, next) => {
   next();
 });
 // Middleware
-//app.use(cors());
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
+
 
 // Handle preflight requests
-app.options('*', cors());
+
 app.use(express.json());
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
